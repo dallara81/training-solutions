ad74a9afe7d800ff3faae6fa11e5fb120d486157
dallara81 <pluggasvenska@yahoo.se>
2020-11-12 00:42:33 +0100

    Song v0.1

---
+++ src/main/java/classstructureattributes/Song.java
@@ -0,0 +1,14 @@
+package classstructureattributes;
+
+public class Song {
+
+    String band;
+    // Előadó, szöveges típus
+
+    String title;
+    // Szám címe, szöveges típus
+
+    int length;
+    // Hossz percben, egész szám (integer) típus
+
+}
