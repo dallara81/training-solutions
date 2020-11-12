7845bdea60b20f6c53ef915cdd47d961f5a222a4
dallara81 <pluggasvenska@yahoo.se>
2020-11-12 05:47:09 +0100

    "Csomagok" c. lecke gyakorlófeladatai

---
+++ src/main/java/packages/greetings/Greeter.java
@@ -0,0 +1,11 @@
+package packages.greetings;
+
+public class Greeter {
+
+    public void sayHello() {
+
+        // Output > Hello World!
+        System.out.println("Hello World!");
+
+    }
+}
---
+++ src/main/java/packages/main/MainProgram.java
@@ -0,0 +1,13 @@
+package packages.main;
+
+import packages.greetings.Greeter;
+
+public class MainProgram {
+
+    // Say Hello!
+    public static void main(String[] args) {
+        new Greeter().sayHello();
+
+    }
+
+}
