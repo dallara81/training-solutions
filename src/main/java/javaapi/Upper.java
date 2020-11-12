98ed9a85b0b265ea96a7cc3f8927757a5541d8ab
dallara81 <pluggasvenska@yahoo.se>
2020-11-12 06:00:45 +0100

    !CSUPA NAGYBETŰ! KONVERTER v0.1

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,17 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+
+        // Head
+        System.out.println("!CSUPA NAGYBETŰ! KONVERTER v0.1");
+        System.out.println("Gyakorlófeladat a Java API leckéhez");
+        System.out.println(" ");
+
+        // Core
+        System.out.println("Hello World!");
+        System.out.println("Hello World!".toUpperCase());
+    }
+
+}
