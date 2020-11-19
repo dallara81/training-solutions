8ce14306088dbe96f761c314074f6e72fb3b1dd3
dallara81 <pluggasvenska@yahoo.se>
2020-11-19 05:59:17 +0100

    Vezérlési szerkezetek - Ciklusok

---
+++ src/main/java/controlselection/greetings/Greetings.java
@@ -0,0 +1,31 @@
+package controlselection.greetings;
+// Napszaktól függő köszönés
+
+
+public class Greetings {
+
+    public static final int MINUTES = 60;
+
+    public String greet(int hour, int minutes) {
+
+        int inMinutes = hour * MINUTES + minutes;
+        if (inMinutes <= 5  * MINUTES || inMinutes > 20 * MINUTES) {
+            return "Jó éjt!";
+        }
+
+        else if (inMinutes > 5 * MINUTES && inMinutes <= 9 * MINUTES) {
+            return "Jó reggelt!";
+        }
+
+        else if (inMinutes > 9 * MINUTES && inMinutes <= 18 * MINUTES + 30) {
+            return "jó napot!";
+        }
+
+        else {
+            return "jó estét!";
+
+        }
+
+    }
+
+}
