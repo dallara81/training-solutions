1e3b6618672a3713564edb2e201d2a158ec55407
dallara81 <pluggasvenska@yahoo.se>
2020-11-24 11:18:30 +0100

    ChangeLetter - Egyedi megoldás 3

---
+++ src/main/java/week05d02/ChangeLetterEgyedi3.java
@@ -0,0 +1,15 @@
+package week05d02;
+
+public class ChangeLetterEgyedi3 {
+
+    static String vowels = "aeiou";
+
+    //
+    // OBS! Max. 15 sor. 34 több, mint 15 :/
+    //
+
+            }
+        }
+        return result;
+    }
+}
