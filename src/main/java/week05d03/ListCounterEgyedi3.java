1e99610991f20910acc95c0ef871a0e774c33a0b
dallara81 <pluggasvenska@yahoo.se>
2020-11-25 13:04:22 +0100

    ListCounter Egyedi 3 - Ez sem volt feladat, de számoljuk meg minden karaktert tételesen úgy, hogy nem tudjuk előre, mik is lesznek benne :D

---
+++ src/main/java/week05d03/ListCounterEgyedi3.java
@@ -0,0 +1,23 @@
+package week05d03;
+
+public class ListCounterEgyedi3 {
+
+    public static void main(String[] args) {
+
+        int wordCount = 0;
+        String word = "Abraka Dabra";
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == letter) {
+                    wordCount++;
+                }
+            }
+            if (wordCount > 0) {
+                System.out.println(letter + "=" + wordCount);
+                wordCount = 0;
+            }
+        }
+    }
+}
+
+
