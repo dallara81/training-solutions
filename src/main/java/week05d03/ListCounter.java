c14b710a2b3aee66c70eb396f4710df54c7603e8
dallara81 <pluggasvenska@yahoo.se>
2020-11-25 13:25:16 +0100

    ListCounter

--- src/main/java/week05d03/ListCounter.java
+++ src/main/java/week05d03/ListCounter.java
@@ -5,13 +5,11 @@ import java.util.ArrayList;
 
 public class ListCounter {
 
-    List<String> list = new ArrayList<>();
-
     public int count(List<String> list) {
         int i = 0;
         for (String s : list) {
             if (s.startsWith("A") || s.startsWith("a")) {
-                i = i + 1;
+                i = i++;
             }
         }
         return i;
