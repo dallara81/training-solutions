50a02e42d4d780b1b8c4a2dfdfe6dcd105e1f86d
dallara81 <pluggasvenska@yahoo.se>
2020-11-27 23:38:17 +0100

    Trimmelő Királykisasszony v0.1

---
+++ src/main/java/dallaramytrimm/Mytrimmer.java
@@ -0,0 +1,19 @@
+package dallaramytrimm;
+
+public class Mytrimmer {
+
+    public static void main(String[] args) {
+
+        System.out.println("Trimmelő Királykisasszony v0.1\n");
+
+        String azalfaomegaja="          Szeretsz a szóközökkel játszani?";
+
+        System.out.println(azalfaomegaja+" Ügyes gyerek vagy!");
+        // trim nélkül
+
+        System.out.println(azalfaomegaja.trim()+" De most már még ügyesebb!");
+        // trimmel (nem a repülőgépet, ember! Ez programozás Trim - mel, nem trim nélkül)
+
+    }
+
+}
