b4d5c6c9bfe6dc7b41a093c91c435d2ad3fc7436
dallara81 <pluggasvenska@yahoo.se>
2020-11-29 17:20:52 +0100

    User v0.0.0

---
+++ src/main/java/week05d05/User.java
@@ -0,0 +1,9 @@
+package week05d05;
+
+// Készíts egy osztályt week05d05.User néven, melynek van egy firstName (String), egy lastName (String) és egy email (String) mezője.
+// Legyen ellenőrzés az User konstruktorában, mely hibát jelez, ha nincs @ és . (pont) az e-mail cimben!
+// Készíts egy metódust is getFullName névvel, mely visszaadja a firstName és a lastName változókat összefűzve.
+// Legyen közöttük egy whitespace (" ").
+
+public class User {
+}
