73f56f895ff5f0ca9a933f4679065a7f7c1ca496
dallara81 <pluggasvenska@yahoo.se>
2020-11-29 18:15:51 +0100

    Példa Petinek - While (01)

---
+++ src/main/java/peldapetinek0001/While01.java
@@ -0,0 +1,14 @@
+package peldapetinek0001;
+
+public class While01 {
+
+    public static void main(String[] args) {
+        // i
+        int i=1;
+        while(i<=8){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
