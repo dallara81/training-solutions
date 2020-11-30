97e2b1b5d47fe989d9aadd00ab40112a90038745
dallara81 <pluggasvenska@yahoo.se>
2020-11-30 15:53:36 +0100

    W06d01 gykorlófeleladat (I. megoldás, lesz több megoldásom is)

---
+++ src/main/java/week06d01/ListSelector.java
@@ -0,0 +1,49 @@
+package week06d01;
+
+import java.util.List;
+
+public class ListSelector {
+
+    public String selectEveryEvenElement(List<String> list) {
+        if (list == null) {
+
+            throw new IllegalArgumentException("Ez a lista ebben a formában nem igazán lesz okés :D");
+
+        }
+
+        String even = "";
+        for (int i = 0; i < list.size(); i += 2) {
+
+            even = even + list.get(i);
+
+        }
+
+        if (!even.equals("")) {
+
+            even = "[" + even + "]";
+
+        }
+
+        return even;
+    }
+
+    public String selectEveryOddElement(List<String> list) {
+        if (list == null) {
+
+            throw new IllegalArgumentException("Ez a lista ebben a formában nem igazán lesz okés :D");
+
+        }
+
+        String odd = "";
+        for (int i = 1; i < list.size(); i += 2) {
+            odd = odd + list.get(i) + ", ";
+        }
+
+        if (!odd.equals("")) {
+            odd = odd.substring(0, (odd.length() - 2));
+            odd = "[" + odd + "]";
+        }
+
+        return odd;
+    }
+}
