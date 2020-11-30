38fafdd3415835c7789445ea939a38ec70897b9c
dallara81 <pluggasvenska@yahoo.se>
2020-11-30 11:49:51 +0100

    For / Loop gyakorló - Ne lopj, Loop-j! Loop-oljunk, ha lehet!

---
+++ src/main/java/floorloopgyakorlo1/ForLoop.java
@@ -0,0 +1,14 @@
+package floorloopgyakorlo1;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        System.out.println("Ne lopj, Loop-j! Loop-oljunk, ha lehet!\n");
+
+        int i;
+        for (i = 0; i <= 12; i++) {
+            System.out.println("Value of i = " + i);
+
+        }
+    }
+}
