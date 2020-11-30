52c57b9c1187d1b5a32671ee404d79068d9ab7e4
dallara81 <pluggasvenska@yahoo.se>
2020-11-30 23:56:16 +0100

    Gyakorlófeladat - Faktoriális-Majális v0.1.0

---
+++ src/main/java/gyakorlofaktorial/GyakorloFaktorial.java
@@ -0,0 +1,16 @@
+package gyakorlofaktorial;
+
+public class GyakorloFaktorial {
+
+    public static void main(String[] args) {
+
+        int num = 7;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
