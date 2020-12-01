1b471f4d91ef65f54980dd7a70614eba08619731
dallara81 <pluggasvenska@yahoo.se>
2020-12-01 03:31:21 +0100

    Biliárd360 játék v0.0.1

---
+++ src/main/java/biliard/Biliard360.java
@@ -0,0 +1,15 @@
+package biliard;
+
+// Biliárd360 játék
+// Célkitűzések:
+// 0.1.x Fizika alapvető működése, akár hibával is. Grafika alapjainak lefektetése, játéktér, golyók csak kóddal megrajzolva
+// 0.2.x Hibjavítások
+// 0.3.x A játék működése tökéletes legyen
+// 0.4.x Hangok bevezetése, valamint a fizika finomítása (pl. asztal meglökése lehetséges legyen)
+// 0.5.x A grafika fejlesztése
+// 0.6.x Menu végelegesítése
+// 0.7.x Mentés lehetősége (opcionális)
+// 1.0.0 Véglegesítés
+
+public class Biliard360 {
+}
