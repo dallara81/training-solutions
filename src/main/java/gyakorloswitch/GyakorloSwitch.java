13b3b7854d4fc4f0dfa996bd80eb732b5d012e3e
dallara81 <pluggasvenska@yahoo.se>
2020-12-02 23:02:30 +0100

    Gyakorló Switch

---
+++ src/main/java/gyakorloswitch/GyakorloSwitch.java
@@ -0,0 +1,33 @@
+package gyakorloswitch;
+
+public class GyakorloSwitch {
+
+    public static void main(String[] args) {
+
+        int day = 5;
+
+        switch (day) {
+            case 1:
+                System.out.println("Hétfő");
+                break;
+            case 2:
+                System.out.println("Kedd");
+                break;
+            case 3:
+                System.out.println("Szerda");
+                break;
+            case 4:
+                System.out.println("Csütörtök");
+                break;
+            case 5:
+                System.out.println("Péntek");
+                break;
+            case 6:
+                System.out.println("Szombat");
+                break;
+            case 7:
+                System.out.println("Vasárnap");
+                break;
+        }
+    }
+}
