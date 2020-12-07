84ade70c96329059de0a8d9cc8a01b63dd7fb147
dallara81 <pluggasvenska@yahoo.se>
2020-12-07 01:20:47 +0100

    Java SE alapok - Java OO elemek > Attribútumok és metódusok > Metódus hívások láncolása

---
+++ src/main/java/methodchain/NavigationPoint.java
@@ -0,0 +1,21 @@
+package methodchain;
+// Metódus hívások láncolása
+// (video-végi feladat)
+
+public class NavigationPoint {
+
+    private int distance;
+    private int azimut;
+
+    public NavigationPoint(int distance, int azimut) {
+        this.distance = distance;
+        this.azimut = azimut;
+
+    }
+
+    @Override
+    public String toString() {
+        return  "distance > " + distance + " azimut > " + azimut;
+
+    }
+}
