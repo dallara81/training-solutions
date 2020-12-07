376b8abe0cb2c0f564358dc03c85945fb876b33c
dallara81 <pluggasvenska@yahoo.se>
2020-12-07 01:49:42 +0100

    Java SE alapok - Java OO elemek > Attribútumok és metódusok > Metódus paraméterek és visszatérési értékek

---
+++ src/main/java/methodparam/ExtremValues.java
@@ -0,0 +1,26 @@
+package methodparam;
+// Metódus paraméterek és visszatérési értékek
+
+
+public class ExtremValues {
+
+    private double min;
+    private double max;
+
+    public ExtremValues(double minimum, double maximum) {
+
+        min = minimum;
+        max = maximum;
+
+    }
+
+    public double getMin() {
+        return min;
+
+    }
+
+    public double getMax() {
+        return max;
+
+    }
+}
