0df33eb09b88dd816e6587672946c2ddaf9cada8
dallara81 <pluggasvenska@yahoo.se>
2020-12-10 15:43:29 +0100

    Week07d04 órai feladatmegoldás (2. megoldás)

---
+++ src/main/java/week07d04/Lab.java
@@ -0,0 +1,49 @@
+package week07d04;
+// Hozz létre egy week07d04.Lab nevű osztályt, ami azt tárolja el, hogy melyik feladattal készültél el.
+// Három attribútuma: title ami a tananyag neve, egy completed, hogy elkészültél-e vele, és a completedAt ,
+// ami azt tárolja, hogy mikor. Legyen két konstruktora!
+// Az egyik csak a címet várja,
+// ekkor úgy állítsa be a példányt, hogy még nem készültél el.
+// Legyen egy másik, ami várja a címet és a dátumot.
+// Ekkor azt állítsa be, hogy elvégezted a gyakorlati feladatot.
+// Írj egy complete metódust, ami kap egy dátumot, és beállítja, hogy elvégezted a gyakorlati feladatot.
+// Legyen egy complete metódusa, ami semmit nem vár, ez azt állítja be, hogy MOST végezted el a gyakorlati feladatot.
+// Írd meg a toString metódusát is!
+
+import java.time.LocalDate;
+
+    public class Lab {
+        private String title;
+        private boolean completed;
+        private LocalDate completedAt;
+
+        public Lab(String title) {
+            this.title = title;
+            this.completed = false;
+        }
+
+        public Lab(String title, LocalDate completedAt) {
+            this.title = title;
+            this.completedAt = completedAt;
+            this.completed = true;
+        }
+
+        public void complete(LocalDate completedAt) {
+            this.completedAt = completedAt;
+            this.completed = true;
+        }
+
+        public void complete() {
+            this.completedAt = LocalDate.now();
+            this.completed = true;
+        }
+
+        @Override
+        public String toString() {
+            return "Subject info: " +
+                    "title='" + title + '\'' +
+                    ", completed=" + completed +
+                    ", completedAt=" + completedAt;
+        }
+    }
+}
