c018821811aed6d087595583babc00a5d657583b
dallara81 <pluggasvenska@yahoo.se>
2020-12-15 08:25:03 +0100

    Week08d02 (Medior)

---
+++ src/main/java/week08d02/Lottery.java
@@ -0,0 +1,7 @@
+package week08d02;
+// Készíts a week08d02 csomagban egy Lottery osztályt, melynek legyen egy getNumbers() metódusa,
+// ami visszaad egy listát amiben 5 véletlen szám van 1-90 között, tehát kvázi készíts egy lottósorsoló programot.
+// A sorrendre nem kell figyelni, a lényeg, hogy a számok különbözők legyenek!
+
+public class Lottery {
+}
