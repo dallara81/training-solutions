7f357b278442810b3f277b7542651c4a186763bc
dallara81 <pluggasvenska@yahoo.se>
2020-12-16 08:31:01 +0100

    Week08day03

---
+++ src/main/java/week08d03/StringLists.java
@@ -0,0 +1,7 @@
+package week08d03;
+// Készíts week08d03 csomagban egy StringLists osztályt és benne egy stringListsUnion(List<String> first,
+// List<String> second) metódust, mely két string listát vár paraméterül és visszaadja az uniójukat.
+// Az úniónak minden elemet tartalmaznia kell mindkét listából, de egy elem nem szerepelhet kétszer!
+
+public class StringLists {
+}
