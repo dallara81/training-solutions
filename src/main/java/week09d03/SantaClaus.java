a3788e31157b841a4c2b71e89afb5bd432448425
dallara81 <pluggasvenska@yahoo.se>
2020-12-23 11:16:58 +0100

    Week09day03 - SantaClaus v0.2

--- src/main/java/week09d03/SantaClaus.java
+++ src/main/java/week09d03/SantaClaus.java
@@ -1,4 +1,26 @@
 package week09d03;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class SantaClaus {
+    private List<Person> personList = new ArrayList<>();
+
+    public SantaClaus(List<Person> personList) {
+        if (personList == null || personList.size() == 0) {
+            throw new IllegalArgumentException("A személyek listája nem lehet üres, vazze!");
+        }
+
+        this.personList = personList;
+    }
+
+    public void getThroughChimneys() {
+        for (Person person : personList) {
+            person.setPresent();
+        }
+    }
+
+    public List<Person> getPersonList() {
+        return personList;
+    }
 }
