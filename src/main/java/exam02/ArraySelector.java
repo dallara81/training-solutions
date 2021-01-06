391f42544f39dab7d628c71071b781d9265f235d
dallara81 <pluggasvenska@yahoo.se>
2021-01-06 22:34:23 +0100

    exam02 - feladat I. és II.

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -2,5 +2,18 @@ package exam02;
 
 public class ArraySelector {
 
-    p
+    public String selectEvens(int[] ints){
+        if (ints.length == 0){
+            return "";
+        }
+
+        StringBuilder finalString = new StringBuilder().append("[");
+        for (int i = 0; i < ints.length; i=i+2) {
+            finalString.append(ints[i]).append(", ");
+        }
+
+        finalString.deleteCharAt(finalString.length()-2).deleteCharAt(finalString.length()-1).append("]");
+        return finalString.toString();
+    }
+
 }
--- src/main/java/exam02/cv/Skill.java
+++ src/main/java/exam02/cv/Skill.java
@@ -1,4 +1,25 @@
 package exam02.cv;
 
 public class Skill {
+
+    private int level;
+    private String name;
+
+    public Skill(String name, int level) {
+        this.level = level;
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + level + ")";
+    }
 }
