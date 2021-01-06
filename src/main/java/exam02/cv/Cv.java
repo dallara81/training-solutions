f27b25d5e88d39575c87359887d0346731a437de
dallara81 <pluggasvenska@yahoo.se>
2021-01-06 22:53:46 +0100

    exam02 (Complete 22:53)

--- src/main/java/exam02/cv/Cv.java
+++ src/main/java/exam02/cv/Cv.java
@@ -1,4 +1,51 @@
 package exam02.cv;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cv {
+
+    private final List<Skill> skills;
+
+    {
+        skills = new ArrayList<>();
+    }
+
+    private final String name;
+
+    public Cv(String name) {
+        this.name = name;
+    }
+
+    public void addSkills(String... cvSkills) {
+        for (String row : cvSkills) {
+            int value = Integer.parseInt(row.substring(row.length() - 2, row.length() - 1));
+            String skillName = row.substring(0, row.length() - 4);
+            skills.add(new Skill(skillName, value));
+        }
+    }
+
+    public int findSkillLevelByName(String skillName) {
+        int skillLevel = 0;
+        for (Skill skill : skills) {
+            if (skill.getName().equals(skillName)) {
+                skillLevel = skill.getLevel();
+            }
+        }
+
+        if (skillLevel == 0) {
+            // WARNING! The Medior group is not here yet!
+            throw new SkillNotFoundException("Exception > I didn't find any skill!");
+        }
+        return skillLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
 }
---
+++ src/main/java/exam02/cv/SkillNotFoundException.java
@@ -0,0 +1,8 @@
+package exam02.cv;
+
+public class SkillNotFoundException extends IllegalStateException {
+
+    public SkillNotFoundException(String message) {
+        super(message);
+    }
+}
