0e6be6a4e88b77b9b458cdf3a1f0c1b4daee092e
dallara81 <pluggasvenska@yahoo.se>
2021-01-06 15:23:26 +0100

    Gyakorló Enum 1. (alap)

---
+++ src/main/java/gyakorloenum1/GyakorloEnum1.java
@@ -0,0 +1,15 @@
+package gyakorloenum1;
+
+public class GyakorloEnum1 {
+
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
+}
