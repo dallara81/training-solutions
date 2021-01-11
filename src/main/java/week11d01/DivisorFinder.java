122c13be24a6ba8febcaf7c2a3252c9e9c4fd3da
dallara81 <pluggasvenska@yahoo.se>
2021-01-11 13:23:42 +0100

    DivisorFinder

---
+++ src/main/java/week11d01/DivisorFinder.java
@@ -0,0 +1,32 @@
+package week11d01;
+
+// Készítsünk egy DivisorFinder nevű osztályt, melynek van egy int findDivisors(int n) metódusa.
+// A feladat az, hogy megnézzük a szám minden egyes számjegyére, hogy osztója-e a számnak,
+// majd számoljuk össze őket. Példa: a 425-ben az 5 osztója a számnak, ezért a visszatérési érték 1.
+
+public class DivisorFinder {
+
+
+    public int findDivisors(int n) {
+        String sn = "" + n;
+        int x;
+        int counter = 0;
+        for (int i = 0; i < sn.length(); i++) {
+            x = Integer.parseInt(sn.substring(i, i + 1));
+            if (x > 0) {
+                if (n % x == 0) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    // Csak azért is main-ben! - mert a main nem Maine
+
+    public static void main(String[] args) {
+        DivisorFinder divisorFinder = new DivisorFinder();
+        System.out.println(divisorFinder.findDivisors(425));
+
+    }
+}
