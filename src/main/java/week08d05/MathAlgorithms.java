a8d4381e36f83ae58d213785204c8a7d933e04ac
dallara81 <pluggasvenska@yahoo.se>
2021-01-23 03:48:36 +0100

    Week08d05

---
+++ src/main/java/week08d05/MathAlgorithms.java
@@ -0,0 +1,28 @@
+package week08d05;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MathAlgorithms {
+
+    private static void isZero(int number1, int number2){
+        if (number1 ==0 || number2 ==0 ){throw  new IllegalArgumentException("A nullával aló osztás értelmetlen!");}
+    }
+
+    private static List<Integer> sort(int number1, int number2){
+        List<Integer> shorted = new ArrayList<>();
+        shorted.add(Math.abs(number1));
+        shorted.add(Math.abs(number2));
+        Collections.sort(shorted);
+        return shorted;}
+
+    public static int greatestCommonDivisor(int number1, int number2){
+        isZero(number1, number2);
+        List<Integer> list = sort(number1, number2);
+        for (int i = list.get(0); i > 1; i-- ){
+            if (list.get(1)% i == 0 && list.get(0)%i == 0){
+                return i;}
+        }
+        return 1;}
+}
