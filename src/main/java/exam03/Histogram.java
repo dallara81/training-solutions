31fcab15591b70881715b9e049c06f27740065e9
dallara81 <pluggasvenska@yahoo.se>
2021-02-15 19:22:17 +0100

    exam03 (Histogram)

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,4 +1,22 @@
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Histogram {
+
+    public String createHistogram(BufferedReader reader) {
+        StringBuilder stars = new StringBuilder();
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                int numberOfStarsPerLine = Integer.parseInt(line);
+                stars.append("*".repeat(numberOfStarsPerLine));
+                stars.append('\n');
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException(" Bobo, seriously... where is the file? I can't read this. ", ioe);
+        }
+        return stars.toString();
+    }
 }
