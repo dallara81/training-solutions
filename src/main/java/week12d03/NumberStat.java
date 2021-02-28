61c7d3444b98c9c7cb168048b9e90c2a0049dc9c
dallara81 <pluggasvenska@yahoo.se>
2021-02-28 10:51:57 +0100

    Week 12 Day 03

---
+++ src/main/java/week12d03/NumberStat.java
@@ -0,0 +1,36 @@
+package week12d03;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NumberStat {
+
+    public int numberFinder(List<Integer> numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            throw new IllegalArgumentException("You must have a list of at least one shitty item!");
+        }
+        List<Integer> orderedNumbers = new ArrayList<>(numbers);
+        Collections.sort(orderedNumbers);
+        int result = orderedNumbers.get(0);
+        int smalest = counter(orderedNumbers.get(0), orderedNumbers);
+        int current = smalest;
+        for (int i = 1; i < orderedNumbers.size(); i++) {
+            if (!orderedNumbers.get(i).equals(orderedNumbers.get(i - 1))) {
+                current = counter(orderedNumbers.get(i), orderedNumbers);
+            }
+            result = current < smalest ? orderedNumbers.get(i) : result;
+        }
+        return result;
+    }
+
+    private int counter(int number, List<Integer> numbers) {
+        int counter = 0;
+        for (int num : numbers) {
+            if (number == num) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
