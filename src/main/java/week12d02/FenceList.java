17152eb4bdc2f4293461d762392a645ba5ee0afc
dallara81 <pluggasvenska@yahoo.se>
2021-02-28 10:55:19 +0100

    Week 12 Day 02

---
+++ src/main/java/week12d02/FenceList.java
@@ -0,0 +1,20 @@
+package week12d02;
+
+public class FenceList {
+
+    private final Fence fence;
+    private final int numberOfSite;
+
+    public FenceList(Fence fence, int numberOfSite) {
+        this.fence = fence;
+        this.numberOfSite = numberOfSite;
+    }
+
+    public Fence getFence() {
+        return fence;
+    }
+
+    public int getNumberOfSite() {
+        return numberOfSite;
+    }
+}
---
+++ src/main/java/week12d02/Site.java
@@ -0,0 +1,26 @@
+package week12d02;
+
+public class Site {
+
+    private final int sideOfStreet;
+    private final int lengthOfSide;
+    private final Fence fenceType;
+
+    public Site(int sideOfStreet, int lengthOfSide, Fence fenceType) {
+        this.sideOfStreet = sideOfStreet;
+        this.lengthOfSide = lengthOfSide;
+        this.fenceType = fenceType;
+    }
+
+    public int getSideOfStreet() {
+        return sideOfStreet;
+    }
+
+    public int getLengthOfSide() {
+        return lengthOfSide;
+    }
+
+    public Fence getFenceType() {
+        return fenceType;
+    }
+}
