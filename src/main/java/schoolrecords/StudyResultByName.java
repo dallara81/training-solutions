783c518a9d3b046d1100dbdef089056a8fda1b88
dallara81 <pluggasvenska@yahoo.se>
2021-03-02 09:05:33 +0100

    2. Projektmunka - Schoolrecords

---
+++ src/main/java/schoolrecords/StudyResultByName.java
@@ -0,0 +1,28 @@
+package schoolrecords;
+// 2. projektmunka
+
+public class StudyResultByName {
+
+    private String studentName;
+    private double studyAverage;
+
+    public StudyResultByName(String studentName, double studyAverage) {
+        this.studentName = studentName;
+        this.studyAverage = studyAverage;
+    }
+
+
+    @Override
+    public String toString() {
+        return
+                studentName + "-" + studyAverage;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public double getStudyAverage() {
+        return studyAverage;
+    }
+}
---
+++ src/main/java/schoolrecords/Subject.java
@@ -0,0 +1,16 @@
+package schoolrecords;
+// 2. projektmunka
+
+public class Subject {
+
+    private String name;
+
+
+    public Subject(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
