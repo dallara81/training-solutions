a977feae8bfab97810b114229794baa4db5951f5
dallara81 <pluggasvenska@yahoo.se>
2021-03-04 04:04:30 +0100

    4. projektmunka - activity

---
+++ src/main/java/activity/Activities.java
@@ -0,0 +1,47 @@
+package activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Activities {
+
+    private final List<Activity> activities;
+
+    public Activities(List<Activity> activities) {
+        this.activities = new ArrayList<>(activities);
+    }
+
+    public void addActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+    public List<Report> distancesByTypes() {
+        List<Report> reports = new ArrayList<>();
+        ActivityType activityType;
+        for (int i = 0; i < ActivityType.values().length; i++) {
+            activityType = ActivityType.values()[i];
+            double distance = 0;
+            for (Activity activity : activities) {
+                if (activity.getType().equals(ActivityType.values()[i])) {
+                    distance += activity.getDistance();
+                }
+            }
+            reports.add(new Report(activityType, distance));
+        }
+        return reports;
+    }
+
+    public int numberOfTrackActivities() {
+        int result = 0;
+        for (Activity activity : activities) {
+            if (activity instanceof ActivityWithTrack) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public int numberOfWithoutTrackActivities() {
+        return activities.size() - numberOfTrackActivities();
+    }
+}
---
+++ src/main/java/activity/Track.java
@@ -0,0 +1,116 @@
+package activity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Track {
+
+    private final List<TrackPoint> trackPoints = new ArrayList<>();
+
+    public void addTrackPoint(TrackPoint trackPoint) {
+        trackPoints.add(trackPoint);
+    }
+
+    public Coordinate findMaximumCoordinate() {
+        emptyListError();
+        double maxLon = trackPoints.get(0).getCoordinate().getLongitude();
+        double maxLat = trackPoints.get(0).getCoordinate().getLatitude();
+        for (TrackPoint trackPoint : trackPoints) {
+            double tLon = trackPoint.getCoordinate().getLongitude();
+            double tLat = trackPoint.getCoordinate().getLatitude();
+            if (tLat > maxLat) {
+                maxLat = tLat;
+            }
+            if (tLon > maxLon) {
+                maxLon = tLon;
+            }
+        }
+        return new Coordinate(maxLat, maxLon);
+    }
+
+    public Coordinate findMinimumCoordinate() {
+        emptyListError();
+        double minLon = trackPoints.get(0).getCoordinate().getLongitude();
+        double minLat = trackPoints.get(0).getCoordinate().getLatitude();
+        for (TrackPoint trackPoint : trackPoints) {
+            double tLon = trackPoint.getCoordinate().getLongitude();
+            double tLat = trackPoint.getCoordinate().getLatitude();
+            if (tLat < minLat) {
+                minLat = tLat;
+            }
+            if (tLon < minLon) {
+                minLon = tLon;
+            }
+        }
+        return new Coordinate(minLat, minLon);
+    }
+
+    public double getDistance() {
+        double result = 0.0;
+        for (int i = 1; i < trackPoints.size(); i++) {
+            result += trackPoints.get(i).getDistanceFrom(trackPoints.get(i - 1));
+        }
+        return result;
+    }
+
+    public double getFullDecrease() {
+        double result = 0.0;
+        for (int i = 1; i < trackPoints.size(); i++) {
+            if (trackPoints.get(i).getElevation() < trackPoints.get(i - 1).getElevation()) {
+                result += trackPoints.get(i - 1).getElevation() - trackPoints.get(i).getElevation();
+            }
+        }
+        return result;
+    }
+
+    public double getFullElevation() {
+        double result = 0.0;
+        for (int i = 1; i < trackPoints.size(); i++) {
+            if (trackPoints.get(i).getElevation() > trackPoints.get(i - 1).getElevation()) {
+                result += trackPoints.get(i).getElevation() - trackPoints.get(i - 1).getElevation();
+            }
+        }
+        return result;
+    }
+
+    public double getRectangleArea() {
+        double width = findMaximumCoordinate().getLongitude() - findMinimumCoordinate().getLongitude();
+        double height = findMaximumCoordinate().getLatitude() - findMinimumCoordinate().getLatitude();
+        return width * height;
+    }
+
+    public List<TrackPoint> getTrackPoints() {
+        return trackPoints;
+    }
+
+    public void loadFromGpx(InputStream is) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+            String trkpt;
+            String ele;
+            double latitude;
+            double longitude;
+            double elevation;
+            while ((trkpt = reader.readLine()) != null) {
+                if (trkpt.contains("<trkpt")) {
+                    latitude = Double.parseDouble(trkpt.substring(15, 25));
+                    longitude = Double.parseDouble(trkpt.substring(32, 42));
+                    ele = reader.readLine();
+                    elevation = Double.parseDouble(ele.substring(9, 14));
+                    trackPoints.add(new TrackPoint(new Coordinate(latitude, longitude), elevation));
+                }
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Nem tudom olvasni a fájlt!", ioe);
+        }
+    }
+
+    private void emptyListError() {
+        if (trackPoints.isEmpty()) {
+            throw new NullPointerException("Üres lista!");
+        }
+    }
+}
