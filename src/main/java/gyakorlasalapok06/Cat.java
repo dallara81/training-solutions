71ecd2704f7ce96a99e8d26ca4ff7ead5046d97a
dallara81 <pluggasvenska@yahoo.se>
2021-03-13 15:06:33 +0100

    Gyakorlás - Alapok - 06 - Polimorfizmus

---
+++ src/main/java/gyakorlasalapok06/Cat.java
@@ -0,0 +1,8 @@
+package gyakorlasalapok06;
+
+public class Cat extends Animal{
+
+    public void meow(){
+        System.out.println("MEOW!");
+    }
+}
---
+++ src/main/java/gyakorlasalapok06/PolimorfizmusNCU.java
@@ -0,0 +1,13 @@
+package gyakorlasalapok06;
+
+public class PolimorfizmusNCU {
+
+    public static void main(String[] args) {
+
+    Cat macska = new Cat();
+    macska.meow();
+    macska.setName("Cirmi");
+    macska.setWeight(5);
+
+    }
+}
