80dfc18f3d8a5b74a68aa07a62af0c990abedbe4
dallara81 <pluggasvenska@yahoo.se>
2021-03-13 17:14:17 +0100

    Gyakorlás - Alapok - 06B - 06 & külső Getter Setter

---
+++ src/main/java/gyakorlosettergetter/GetterSetterCat.java
@@ -0,0 +1,16 @@
+package gyakorlosettergetter;
+
+import gyakorlasalapok06.Cat;
+
+public class GetterSetterCat {
+
+    public static void main(String[] args) {
+
+        Cat macska = new Cat();
+
+        macska.setName("Girhes");
+
+        System.out.println(macska.getName());
+
+    }
+}
