204c761116c5360abfd1fa0436103b6994f3f09e
dallara81 <pluggasvenska@yahoo.se>
2021-03-13 17:58:05 +0100

    Gyakorlás - Alapok - String-é alakítás valamilyen szám típusból

---
+++ src/main/java/gyakorlotipusatalakitasok/TipusAtalakitasok.java
@@ -0,0 +1,13 @@
+package gyakorlotipusatalakitasok;
+// String-é alakítás valamilyen szám típusból
+
+public class TipusAtalakitasok {
+
+    public static void main(String[] args) {
+
+        char first = 'b';
+        int second = 2;
+        String third = "" + first;
+        third = "" + second;
+    }
+}
