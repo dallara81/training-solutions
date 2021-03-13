cc56e613f0d6d2d88265b741d2bf424d6eec8465
dallara81 <pluggasvenska@yahoo.se>
2021-03-13 11:57:06 +0100

    Gyakorlas - Alapok - 01 - println

---
+++ src/main/java/gyakorlasalapok01/GyakorloPrint.java
@@ -0,0 +1,13 @@
+package gyakorlasalapok01;
+
+
+public class GyakorloPrint {
+    public static void main(String[] args) {
+        System.out.println("Print Line gyakorló - GYÉPÉSEKNEK\n");
+        System.out.println("Ebben a baszásban Szöveg +2 +2 eredményét láthadod:");
+        System.out.println("Szöveg" + 2 + 2);
+        System.out.println("\n Ebben a bazsásban pedig a 2 + 2 + Szöveg eredménye látható:");
+        System.out.println(2 + 2 + "Szöveg");
+    }
+
+}
