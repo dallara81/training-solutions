2303487c8b4c0dcb439973b485525a30e635cfef
dallara81 <pluggasvenska@yahoo.se>
2021-03-15 20:36:16 +0100

    Tesztek

--- src/test/java/constructoroverloading/BusTimeTableTest.java
+++ src/test/java/constructoroverloading/BusTimeTableTest.java
@@ -1,8 +1,9 @@
 package constructoroverloading;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
