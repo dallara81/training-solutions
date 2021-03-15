37c7e35767ef4b54b6b9548c39abebcb2fb63abd
dallara81 <pluggasvenska@yahoo.se>
2021-03-15 15:18:34 +0100

    Sentence Generator Game (SF)

---
+++ src/main/java/sentencegenerator/Game.java
@@ -0,0 +1,23 @@
+package sentencegenerator;
+
+public class Game {
+
+    public String generator(){
+
+        String[] firstWord = {"Nagyon jó","Megbízható","Kiváló","Tökéletes","A legeslegjobb"};
+        String[] secondWord = {"megoldás","lehetőség","kivitelezés"};
+        String[] thirdWord = {"neked!","mindenkinek!","az egész világnak!","még a földönkívülieknek is!"};
+
+        int firstLength = firstWord.length;
+        int secondLength = secondWord.length;
+        int thirdLength = thirdWord.length;
+
+        int random1 = (int) (Math.random() * firstLength);
+        int random2 = (int) (Math.random() * secondLength);
+        int random3 = (int) (Math.random() * thirdLength);
+
+        String sentence = firstWord[random1] + " " + secondWord[random2] + " " + thirdWord[random3];
+
+        return sentence;
+    }
+}
---
+++ src/main/java/sentencegenerator/SentenceGenerator.java
@@ -0,0 +1,10 @@
+package sentencegenerator;
+
+public class SentenceGenerator {
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        System.out.println(game.generator());
+    }
+}
